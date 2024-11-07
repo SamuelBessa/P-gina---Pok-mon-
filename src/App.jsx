f6584f57348d18b90e315e72fb11eb2b22feed8c
Samuel Bessa <Dev.samuelbessa@gmail.com>
2024-11-06 23:09:56 -0300

    tirando a meta que foi add no index.html

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <RouterProvider router={router} basename={process.env.PUBLIC_URL}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
